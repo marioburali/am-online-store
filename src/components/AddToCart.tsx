bb3b183e62d89f6fcd000cb0f6ff3b98d5694fc8
angelofonseca <angeloniemeyer97@gmail.com>
2024-12-08 14:07:09 -0300

    Ajuste no ProductList e ProductCard Remove estilizaçoes do card e da transição do botao de adicionar ao carrinho

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -8,37 +8,19 @@ import useCart from '../hooks/useCart';
 
 function AddToCart({ product }: AddToCartProps) {
   const { addToCart } = useCart();
-  const [isAdded, setIsAdded] = useState(false);
-
-  useEffect(() => {
-    if (isAdded) {
-      const timer = setTimeout(() => {
-        setIsAdded(false);
-      }, 1000);
-
-      return () => clearTimeout(timer);
-    }
-  }, [isAdded]);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
     addToCart(product);
-    setIsAdded(true);
   };
 
   return (
     <Button
-      variant={ isAdded ? 'success' : 'gradient' }
+      variant="gradient"
       onClick={ handleClick }
-      className={ `buttonCart w-full transition-all duration-300 ${isAdded ? 'disabled:opacity-100 disabled:cursor-not-allowed' : ''}` }
-      disabled={ isAdded }
+      className="buttonCart w-full"
     >
-      {isAdded ? (
-        <div className="flex justify-evenly grow">
-          <p>Item Adicionado</p>
-          <FaCheck />
-        </div>
-      ) : 'Adicionar ao carrinho'}
+      Adicionar ao carrinho
     </Button>
   );
 }
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,27 +1,17 @@
-import { NavLink } from 'react-router-dom';
-import { ProductsListProps } from '../Types';
-import { createURLSlug } from '../helpers/createURLSlug';
 import ProductCard from './ProductCard';
-import AddToCart from './AddToCart';
 import { useProductContext } from '../context/ProductContext';
 
-function ProductsList({ showAddToCart = true }: ProductsListProps) {
-  const { products, isSearched, saveProduct } = useProductContext();
+function ProductsList() {
+  const { products, isSearched } = useProductContext();
   if (products.length === 0 && isSearched) {
     return <h2>Nenhum produto foi encontrado</h2>;
   }
   return (
     products.map((product) => (
-      <NavLink
-        to={ `/product/${createURLSlug(product.title)}` }
-        key={ product.id }
-        onClick={ () => saveProduct(product) }
-      >
-        <div className="card hover:bg-gray-100 hover:shadow-lg transition-all duration-300">
-          <ProductCard product={ product } isDetailedView={ false } />
-          {showAddToCart && (<AddToCart product={ product } />)}
-        </div>
-      </NavLink>))
+      <div key={ product.id } className="card hover:bg-gray-100 hover:shadow-lg transition-all duration-300">
+        <ProductCard product={ product } isDetailedView={ false } />
+      </div>
+    ))
   );
 }
 
